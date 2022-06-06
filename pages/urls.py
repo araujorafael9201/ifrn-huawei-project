e2b6b9f0d62abd25e872d798d35f7f7ce0f3cbf5
Rafael <dev.rafaelaraujo@gmail.com>
2022-06-06 18:58:16 -0300

    Adicionando Páginas

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='indexs')
+    path('', views.index, name='indexs'),
+    path('sobre_ict_academy', views.sobre_ict_academy, name='sobre_ict_academy'),
+    path('sobre_huawei', views.sobre_huawei, name='sobre_huawei'),
+    path('cloud_service', views.cloud_service, name='cloud_service'),
+    path('inteligencia_artificial', views.inteligencia_artificial, name='inteligencia_artificial'),
+    path('datacom', views.datacom, name='datacom'),
+    path('5g', views.cincog, name='5g')
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def sobre_ict_academy(request):
+    return render(request, 'pages/sobre_ict_academy.html')
+
+def sobre_huawei(request):
+    return render(request, 'pages/sobre_huawei.html')
+
+def cloud_service(request):
+    return render(request, 'pages/cloud_service.html')
+
+def inteligencia_artificial(request):
+    return render(request, 'pages/inteligencia_artificial.html')
+
+def datacom(request):
+    return render(request, 'pages/datacom.html')
+
+def cincog(request):
+    return render(request, 'pages/5g.html')
