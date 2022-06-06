ac9a07be6c1979bf3bdbd39f169e3ea831c14949
Pedro Costa <pedrohdcosta@outlook.com>
2022-06-06 18:52:10 -0300

    Adicionando algumas páginas

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,17 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='indexs')
+    path('', views.index, name='index'),
+
+    # EVENTOS
+    path('ict_job_fair_brasil/', views.ict_job_fair_brasil, name='ict_job_fair_brasil'),
+    path('ict_competition_brasil/', views.ict_competition_brasil, name='ict_competition_brasil'),
+    path('seeds_for_the_future/', views.seeds_for_the_future, name='seeds_for_the_future'),
+
+    path('laboratorios/', views.laboratorios, name='laboratorios'),
+    path('instrutores/', views.instrutores, name='instrutores'),
+    path('certificacao/', views.certificacao, name='cerficacao'),
+    path('contato/', views.contato, name='contato'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
+# EVENTOS
+def ict_job_fair_brasil(request):
+    return render(request, 'pages/ict-job-fair-brasil.html')
+
+def ict_competition_brasil(request):
+    return render(request, 'pages/ict-competition-brasil.html')
+
+def seeds_for_the_future(request):
+    return render(request, 'pages/seeds-for-the-future.html')
+
+
+
 def index(request):
     return render(request, 'pages/index.html')
+
+def instrutores(request):
+    return render(request, 'pages/instrutores.html')
+
+def certificacao(request):
+    return render(request, 'pages/certificacao.html')
+
+def contato(request):
+    return render(request, 'pages/contato.html')
+
+def laboratorios(request):
+    return render(request, 'pages/laboratorios.html')
