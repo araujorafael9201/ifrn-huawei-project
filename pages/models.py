0c58c6960a71f23f1931e923ee7654490ed43ce2
Pedro Costa <pedrohdcosta@outlook.com>
2022-06-08 16:00:17 -0300

    Atualizando os models

--- pages/models.py
+++ pages/models.py
@@ -1,33 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Turma(models.Model):
+    curso = models.CharField(max_length=30)
 
 class Aluno(models.Model):
-    cpf = models.CharField(max_length=10)
-    nome = models.CharField(max_length=50)
-    genero = models.CharField(max_length=10)
-    formacao = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=11)
+    nome = models.CharField(max_length=255)
+    genero = models.CharField(max_length=9)
+    formacao = models.CharField(max_length=255)
     nascimento = models.DateField()
     aluno_do_ifrn = models.BooleanField()
     servidor_do_ifrn = models.BooleanField()
 
     email = models.EmailField()
-    celular = models.CharField(max_length=15)
+    celular = models.CharField(max_length=11)
 
     # Senha ainda está como texto simples
-    senha = models.CharField(max_length=20)
+    senha = models.CharField(max_length=255)
 
-    turmas = models.ForeignKey('Turma', on_delete=models.CASCADE)
-    
-    # Faltando arquivo de comprovação de conhecimento
-    #comprovacao_conhecimento = models.FileField() ?
-
-class Turma(models.Model):
-    curso = models.CharField(max_length=30)
-    # estado dos alunos?
+    turmas = models.ForeignKey(Turma, on_delete=models.DO_NOTHING)
+    comprovacao_conhecimento = models.FileField(upload_to=f'pdfs/user_{0}'.format(cpf))
 
 class Professor(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
     # Senha ainda está como texto simples
-    senha = models.CharField(max_length=20)
-    turma = models.OneToOneField('Turma', on_delete=models.SET_DEFAULT, default='Sem Professor')
+    senha = models.CharField(max_length=255)
+    turma = models.OneToOneField(Turma, on_delete=models.SET_DEFAULT, default='Sem Professor')
